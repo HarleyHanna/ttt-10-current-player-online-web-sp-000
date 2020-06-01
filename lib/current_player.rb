6994541e14fa92eefefe02cfd0ed14de3c9793bf
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-01 20:53:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,8 +4,8 @@ def turn_count(board)
   turn_number = 0
   board.each do |turn|
     if turn != " "
+      puts "It is turn number #{turn_number}"
       turn_number += 1
-      return turn_number
     end
     return turn_number
   end
