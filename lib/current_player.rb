9a06b25e872da5e99c7b69ef3a27ccde135ae1bc
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-01 20:42:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,11 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
   turn_number = 0
-  board.each {|turn|
+  board.each do |turn|
     if board[turn] != " "
       turn_number +=
     else
       puts "It is now turn number #{turn_number}"
-    end}
+    end
+  end
 end
