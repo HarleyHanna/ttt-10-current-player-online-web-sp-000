a0d725d28ab811d89cd2bb25de74557d3a153581
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-01 20:24:37 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,6 @@ board [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   turn_count = 0
   board.each |turn|
-    if 
-    puts "It is turn number #{turn}"
+    if board[turn] == "X" || if board[turn] == "O"
+      turn_count +=
 end
