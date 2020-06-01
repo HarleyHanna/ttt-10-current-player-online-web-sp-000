6198e3d4a355de31c07b56101d5b5dfd595cfc35
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-01 20:29:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,5 @@ board [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   turn_number = 0
   board.each |turn|
-    if board[turn] == "X" || if board[turn] == "O"
       turn_number +=
-    end
 end
